a022e9b4e9adc1e5611b49ab4211b02d1c17e6d2
Mário Neto <100155789+mariofneto@users.noreply.github.com>
2022-05-09 11:28:37 -0300

    acab0ou

---
+++ exsobrefunçõesearrays/script.js
@@ -0,0 +1,14 @@
+function adicionar() {
+    let num = document.getElementById('num');
+    let res = document.getElementById('res');
+
+    if (num.value < 1 || num.value > 100){
+        window.alert('Digite um valor válido');
+    }else{
+        let valor = Number(num.value);
+        let c = 1
+        while ( c <= valor){
+
+        }
+    }
+}
